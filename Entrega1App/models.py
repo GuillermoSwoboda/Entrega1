7a6309e888da9d3dc459bc950d627e5886dd8c9a
GuillermoSwoboda <guilleswobo@hotmail.com>
2022-06-26 21:04:24 -0300

    creacion de modelos

---
+++ Entrega1App/models.py
@@ -0,0 +1,29 @@
+from tabnanny import verbose
+from django.db import models
+
+# Create your models here.
+
+class Marcas(models.Model):
+    marca = models.CharField("Marca",max_length=30)
+    CATEGORIAS = (
+        (1, "Bazar"),
+        (2, "Camping"),
+        (3, "Rodados"),
+        (4, "Deportes"),
+        (5, "Ferretería"),
+    )
+    categoria = models.PositiveSmallIntegerField("Categoría",choices=CATEGORIAS)
+    cantidad_productos = models.IntegerField("Cantidad de productos")
+
+class Productos(models.Model):
+    nombre = models.CharField(max_length=30) 
+    CATEGORIAS = (
+        (1, "Bazar"),
+        (2, "Camping"),
+        (3, "Rodados"),
+        (4, "Deportes"),
+        (5, "Ferretería"),
+    )
+    categoria = models.PositiveSmallIntegerField("Categoría",choices=CATEGORIAS)
+    precio = models.FloatField("Precio $")
+    stock_disponible = models.IntegerField("Stock disponible")
