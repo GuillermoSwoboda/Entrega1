a458faaff98565f7243a2eb9c5676062adabb2b0
GuillermoSwoboda <guilleswobo@hotmail.com>
2022-06-28 10:48:21 -0300

    actualizacion modelos

--- Entrega1App/models.py
+++ Entrega1App/models.py
@@ -31,3 +31,26 @@ class Productos(models.Model):
     stock = models.IntegerField("Stock")
     class Meta:
         verbose_name_plural = "Productos"
+        
+class Locales(models.Model):
+    nombre = models.CharField(max_length=50) 
+    LOCALIDAD = (
+        (1, "Córdoba"),
+        (2, "Capital Federal"),
+        (3, "Rosario"),
+        (4, "Mendoza"),
+        (5, "Salta"),
+    )
+    localidad = models.PositiveSmallIntegerField("Localidad",choices=LOCALIDAD)
+    RUBROS = (
+        (1, "Especialista en Bazar"),
+        (2, "Especialista en Camping"),
+        (3, "Especialista en Rodados"),
+        (4, "Especialista en Deportes"),
+        (5, "Especialista en Ferretería"),
+    )
+    rubro_principal = models.PositiveSmallIntegerField("Rubro",choices=RUBROS)
+    horario_apertura = models.TimeField("Apertura")
+    horario_cierre = models.TimeField("Apertura")
+    class Meta:
+        verbose_name_plural = "Locales"
--- Entrega1App/urls.py
+++ Entrega1App/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     
     # path("",inicio, name="inicio"),
-    path("",views.index, name="index")    
+    path("",views.index, name="index"),    
     # # URLS de Entrega1App
     # path("profesores/", profesores, name="profesores"),
     # path("estudiantes/", estudiantes, name="estudiantes"),
