74ec474b4fe6d5276d8a4081646b5d605d33d2d8
GuillermoSwoboda <guilleswobo@hotmail.com>
2022-07-25 19:35:33 -0300

    actualizar readme

--- Entrega1/settings.py
+++ Entrega1/settings.py
@@ -31,8 +31,6 @@ SECRET_KEY = 'django-insecure-h9$k=a*p=q$(=l!5^#hcnw)e7r8&e*jv$0m&2**98@cmi%-f08
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'https://lit-basin-97364.herokuapp.com/',
-    'lit-basin-97364.herokuapp.com',
 ]
 
 
